ede841d33e4a4d3c87f922a8bfe128aa4d3fd7bd
OscarSantiago45 <osantiago@LAPTOP-EO30SEV9.localdomain>
2021-08-12 16:22:34 -0500

    eliminando console.log y borrando espacios innecesarios

--- figuras.js
+++ figuras.js
@@ -1,57 +1,37 @@
 //Codigo del cuadrado
-
-console.group("Menjases del cuadrado");
-//console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`);
-
 function PerimetroCuadrado(lado){
     return lado*4;
 }
-
-//console.log(`El Perimetro del cuadrado es: ${PerimetroCuadrado()} cm`);
 
 function AreaCuadrado(lado){
     return lado*lado;
 } 
 
-//console.log(`El Area del Cuadrado es: ${AreaCuadrado} cm2`);
-console.groupEnd(); 
-
 //Codigo del Triangulo
-
-console.group("Triangulo");
-//console.log(`Los lados del trinagulo miden: ${ladoTriangulo1} cm, ${ladoTriangulo2} cm, ${baseTriangulo} cm`);
-
 function perimetroTriangulo(lado1,lado2,base){
     resultado= lado1+lado2+base;
     return resultado;
 }
-//console.log(`El perimetro del triangulo es: ${perimetroTriangulo} cm`);
 
 function calcularAlturaI(lado1,lado2,base){
     if(lado1===lado2){
         var valor1= (base*base)/2;
         var resultado= Math.sqrt(valor1);
         return resultado;
-        //console.log(`La Altura es: ${resultado}`);
-
-
     }
     else if(lado2===base){
         var valor1= (lado1*lado1)/2;
         var resultado= Math.sqrt(valor1);
         return resultado;
-        //console.log(`La Altura es: ${resultado}`);
     }
     else if( lado1===base){
         var valor1= (lado2*lado2)/2;
         var resultado= Math.sqrt(valor1);
         return resultado;
-        //console.log(`La Altura es: ${resultado}`);
     }
     else{
         var resultado= "No es un triangulo Isosceles";
         return resultado;
-        //console.log("No Es un triangulo Isoseles")
     }
 }
 
@@ -59,27 +39,22 @@ function areaTriangulo(base,altura){
     return (base*altura)/2 ;
 }
 
-//console.log(`El Area del triangulo es: ${areaTriangulo} cm2`);
-console.groupEnd();
-
 //Codigo del Circulo
-console.group("Ciruculo");
-
 function diametroCirculo(radioCirulo){
     return radioCirulo*2;
 };
-//console.log(`El Diametro del circulo es: ${diametroCirculo}`);
+
 const PI= Math.PI;
 
 function perimetroCirculo(radio){
     const diametro= diametroCirculo(radio);
     return diametro*PI;
 }
-//console.log(`El Perimetro del Circulo es: ${perimetroCirculo}`);
+
 function areaCirculo(radio){
     return (radio*radio)*PI;
 }
-//console.log(`El Area del circulo es: ${areaCirculo}`);
+
 
 function calcularPerimetroCuadrado(){
     const input= document.getElementById("InputCuadrado");
